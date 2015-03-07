448a49a103496d52b27a7012ea4a9e8ab1a221b9
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-03-07 12:08:21 +0800

    Updates

--- app/views/listing/frame.blade.php
+++ app/views/listing/frame.blade.php
@@ -1,22 +1,22 @@
 <div class="col-md-10 col-md-offset-1">
     {{--Background Images--}}
-    <div style="background: #FCFCFC;" >
+    <div style="background: #FCFCFC;" class="hidden-xs" >
         <img src="{{ $product->present()->image }}" width="100%" height="500"
              class="reel img-responsive elem-center"
-             id="image"
              data-opening="1"
              data-delay="5"
              data-speed=".2"
              data-images="{{ $product->present()->imgSequence }}" />
-        {{--<img id="product_background"--}}
-            {{--src='{{ $product->present()->image }}' class="img-responsive elem-center" />--}}
-
-        {{--Design Image--}}
+    </div>
 
 
-        {{--Frame Image--}}
-
-
+    <div style="background: #FCFCFC;" class="visible-xs" >
+        <img src="{{ $product->present()->image }}" width="100%" height="250"
+             class="reel img-responsive elem-center"
+             data-opening="1"
+             data-delay="5"
+             data-speed=".2"
+             data-images="{{ $product->present()->imgSequence }}" />
     </div>
 
 
