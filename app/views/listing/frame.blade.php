7bc8b2ae7afea89b4d97d1a7ca015e6ff2138e56
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-03-07 12:13:25 +0800

    Updates

--- app/views/listing/frame.blade.php
+++ app/views/listing/frame.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-10 col-md-offset-1">
     {{--Background Images--}}
     <div style="background: #FCFCFC;" >
-        <img src="{{ $product->present()->image }}" width="100%" height="500"
+        <img src="{{ $product->present()->image }}" width="100%" height="600"
              class="reel img-responsive elem-center"
              data-opening="1"
              data-delay="5"
