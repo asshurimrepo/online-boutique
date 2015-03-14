a5cab9f40d68f81e7af08ec0ac330daf2c728d1e
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-03-15 04:37:54 +0800

    Updates

--- app/routes/routes_admin.php
+++ app/routes/routes_admin.php
@@ -1,5 +1,14 @@
 <?php
 
+	//Contact
+	Route::group( [ 'before' => 'auth', 'prefix' => 'admin/contact', 'namespace' => 'Admin' ], function () {
+		Route::get('/', [ 'before' => 'auth', 'as' => 'contacts.index', 'uses' => 'ContactController@index' ]);
+		Route::get('edit/{id}', [ 'before' => 'auth', 'as' => 'contacts.edit', 'uses' => 'ContactController@edit' ]);
+		Route::post('store', [ 'before' => 'auth', 'as' => 'contacts.store', 'uses' => 'ContactController@store' ]);
+		Route::post('update', [ 'before' => 'auth', 'as' => 'contacts.update', 'uses' => 'ContactController@update' ]);
+		Route::post('delete', [ 'before' => 'auth', 'as' => 'contacts.delete', 'uses' => 'ContactController@destroy' ]);
+	} );
+
 Route::group( [ 'before' => ['auth', 'auth.admin.only'], 'prefix' => 'admin', 'namespace' => 'Admin' ], function (){
 
     Route::get('account', ['as' => 'admin.dashboard.index', 'uses' => 'DashboardController@index' ]);
