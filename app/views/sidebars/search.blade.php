547c2f3116fd920700bd0f592ae790fe764a54f3
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-03-15 10:12:54 +0800

    Updates

--- app/views/sidebars/search.blade.php
+++ app/views/sidebars/search.blade.php
@@ -10,19 +10,7 @@
     </div>
 </div>
 
-<div class="filter-item ">
-    <div class="filter-heading clearfix"><h4>Price Range</h4></div>
-    <div ng-controller="PriceSliderController">
-        <slider floor="0" ceiling="1000" ng-model-low="lower_price_bound" ng-model-high="upper_price_bound"></slider>
-        <div class="clearfix space-xs">
-            <div class="pull-left">@{{lower_price_bound}} </div>
-            <div class="pull-right">@{{upper_price_bound}} </div>
-        </div>
-        {{--Price Range Values--}}
-        <input type="hidden" name="price_min" value="@{{lower_price_bound}}"/>
-        <input type="hidden" name="price_max" value="@{{upper_price_bound}}"/>
-    </div>
-</div>
+
 
 <div class="filter-item border-bottom">
     <div class="filter-heading clearfix"><h4>Category</h4> {{--<button class="btn">Reset</button>--}}</div>
